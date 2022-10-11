1034f4a1d8985e73be774e18dc796ee3f1178ae3
ChadJad <e1929936@cmaisonneuve.qc.ca>
2022-10-11 08:45:03 -0700

    fixed comments get

--- routes/comments.js
+++ routes/comments.js
@@ -11,13 +11,8 @@ router.get('/:id', async (req, res) => {
     try {
         const id = req.params.id
         const resultat = await request.getComments(id)
-
-        if(resultat.length!=0)
-		{   
-			res.send(resultat);
-		} else {
-			res.send({result : 'error'});
-		}
+		res.send(resultat);
+		
 	} catch (error) {
 		res.status(500).json(error.message);
 	}
